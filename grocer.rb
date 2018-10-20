c4a5ddec406c9e1e7a5a0e156ecf67bb267c3177
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-10-20 21:41:03 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -24,6 +24,7 @@ def apply_coupons(cart, coupons)
           cart_item_info[:count] -= discounts[:num]
           hash["#{cart_item} W/COUPON"] = {:price => discounts[:cost], :clearance => cart_item_info[:clearance]}
         end 
+          if hash.has_key?()
          hash["#{cart_item} W/COUPON"].merge!({:count => counter})
       end 
     end 

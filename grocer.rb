036c7ec87f742ca0df09cc5ef162bb7e71b35f78
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-10-20 21:15:28 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -23,7 +23,7 @@ def apply_coupons(cart, coupons)
         puts discounts[:num]
         while cart_item_info[:count] > discounts[:num]
           puts "Made it into the while loop"
-          cart_item_info[:count] - discounts[:num]
+          cart_item_info[:count] -= discounts[:num]
           counter += 1
           cart["#{cart_item} W/COUPON"] = {:price => discounts[:cost], :clearance => cart_item_info[:clearance]}
         end 

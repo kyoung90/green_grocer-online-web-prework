eb6c688438087a4acf1737bb1d8b295e6e18c59c
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-10-20 21:09:29 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,35 @@
 def consolidate_cart(cart)
   # code here
+  new_hash = Hash.new(0)
+  cart.each do |hash|
+    hash.each do |cart_item, cart_item_info|
+      if !new_hash.has_key?(cart_item)
+        new_hash[cart_item] = cart_item_info.merge({:count => 1})
+      else
+        new_hash[cart_item][:count] += 1
+      end 
+    end 
+  end 
+  return new_hash
 end
 
 def apply_coupons(cart, coupons)
   # code here
+  counter = 0
+  cart.each do |cart_item, cart_item_info|
+    coupons.each do |discounts|
+      if cart_item == discounts[:item] 
+        while cart_item_info[:count] > discounts[:num]
+          cart_item_info[:count] - discounts[:num]
+          counter += 1
+          cart["#{cart_item} W/COUPON"] = {:price => discounts[:cost], :clearance => cart_item_info[:clearance]}
+        end 
+        puts "cart with coupon"
+        puts cart["#{cart_item} W/COUPON"]
+         cart["#{cart_item} W/COUPON"].merge!({:count => counter})
+      end 
+    end 
+  end 
 end
 
 def apply_clearance(cart)

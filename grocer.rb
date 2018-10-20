b017e3714abcc55667daf0f19898ba4b4876c2f0
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-10-20 22:07:58 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -39,6 +39,7 @@ def apply_clearance(cart)
   # code here
   puts cart
   cart.each |cart_item, cart_item_info|
+    puts "cart_item_info #{cart_item_info}"
     if cart_item_info[:clearance]
       cart_item_info[:price] *= 0.80
     end 
